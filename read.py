48a156c19d7728438d7d7a8e15aeab9b4f1a5947
Yash Patel <ypatel@princeton.edu>
2017-12-02 00:39:20 -0500

    Added rectangle formatting

--- read.py
+++ read.py
@@ -11,6 +11,28 @@ import numpy as np
 def extract_board(img):
     pass
 
+def convert_form(feature):
+    # order has to be:
+    # [top_left, top_right, bottom_right, bottom_left]
+    sums = [sum(pt[0]) for pt in feature]
+    unused = {0,1,2,3}
+
+    top_left_ind = np.argmin(sums)
+    bottom_right_ind = np.argmax(sums)
+
+    top_left = feature[top_left_ind]
+    
+    top_left_dist = [(abs(top_left[0][1] - pt[0][1]), i) 
+        for i, pt in enumerate(feature)]
+    top_right_ind = sorted(top_left_dist)[1][1]
+
+    unused.remove(top_left_ind)
+    unused.remove(bottom_right_ind)
+    unused.remove(top_right_ind)
+    bottom_left_ind = list(unused)[0]
+    return np.array([feature[top_left_ind],feature[top_right_ind],
+            feature[bottom_right_ind],feature[bottom_left_ind]])
+
 def main(fn):
     img    = cv2.imread(fn)
     gray   = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -34,7 +56,11 @@ def main(fn):
     
     areas.sort(reverse=True)
     board_contour = contours[areas[1][1]]
-    cv2.drawContours(img, [board_contour], 0, (0,255,0), 3)
+    board_perim  = cv2.arcLength(board_contour, True)
+    best_rect = cv2.approxPolyDP(board_contour, .02 * board_perim, True)
+    form_rect = convert_form(best_rect)
+
+    cv2.drawContours(img, [form_rect], 0, (0,255,0), 3)
     cv2.imwrite("result.jpg", img)
 
 if __name__ == "__main__":
