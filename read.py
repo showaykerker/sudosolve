d6d4ad4a8f1da22db9269b8da38cea6cf7abd7c9
Yash Patel <ypatel@princeton.edu>
2017-12-02 11:35:04 -0500

    Added prediction to board (buggy)

--- read.py
+++ read.py
@@ -8,6 +8,12 @@ that is present in the image
 import cv2
 import numpy as np
 
+from digit import get_model
+
+num_squares = 9
+img_rows = 28
+img_cols = 28
+
 def convert_form(feature):
     # order has to be:
     # [top_left, top_right, bottom_right, bottom_left]
@@ -56,25 +62,37 @@ def extract_board(img):
     best_rect = cv2.approxPolyDP(board_contour, .02 * board_perim, True)
     form_rect = convert_form(best_rect)
 
-    transform_dest = np.array([[0,0],[251,0],[251,251],[0,251]], np.float32)
+    transform_width, transform_height = img_rows * num_squares, img_cols * num_squares
+    transform_dest = np.array([[0,0],[transform_width-1,0],
+            [transform_width-1,transform_height-1],[0,transform_height-1]], np.float32)
     transfrom = cv2.getPerspectiveTransform(form_rect,transform_dest)
-    return cv2.warpPerspective(gray,transfrom,(252,252))
+    return cv2.warpPerspective(gray,transfrom,
+        (transform_width,transform_height))
 
 def main(fn):
     img = cv2.imread(fn)
     board = extract_board(img)
+    model = get_model()
 
+    EMPTY_THRESH = 0.80
     text_board = [[] for _ in range(9)]
     for i in range(9):
         for j in range(9):
-            box = board[(i*28):((i+1)*28),(j*28):((j+1)*28)]
+            box = board[(i*img_rows):((i+1)*img_rows),(j*img_cols):((j+1)*img_cols)]
             blur_box = cv2.GaussianBlur(box, (5,5),0)
-            thresh = cv2.adaptiveThreshold(blur_box,255,
+            
+            thresh = cv2.adaptiveThreshold(blur_box,1,
                 cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+            thresh = thresh.reshape(1, img_rows, img_cols, 1)
+            
+            frac_bg = np.mean(thresh)
+            if frac_bg > EMPTY_THRESH:
+                digit = 0
+            else:
+                digit = np.argmax(model.predict(thresh))
+            text_board[i].append(digit)
 
-            if i == 1 and j == 0:
-                cv2.imwrite("test.jpg", thresh)
-
+    print(np.array(text_board))
     cv2.imwrite("result.jpg", board)
 
 if __name__ == "__main__":
